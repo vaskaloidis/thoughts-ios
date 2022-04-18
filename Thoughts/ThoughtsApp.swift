7a7413b2b72d871d9ccc5275bff15502009f69b4
Vas Kaloidis <vas.kaloidis@gmail.com>
2022-04-17 22:29:00 -0400

    Initial Commit

---
+++ Thoughts/ThoughtsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThoughtsApp.swift
+//  Thoughts
+//
+//  Created by Vas Kaloidis on 4/17/22.
+//
+
+import SwiftUI
+
+@main
+struct ThoughtsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
